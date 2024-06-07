e2e85b4d96f51852e6ea76981f63b80f0004aa00
Manuel Valdepeñas <manuelvdlt99@gmail.com>
2024-06-07 12:35:02 +0200

    Log4J is not needed anymore

--- arqueras_javafx/arqueras_javafx/src/main/java/com/gp/arqueras_javafx/GameOverController.java
+++ arqueras_javafx/arqueras_javafx/src/main/java/com/gp/arqueras_javafx/GameOverController.java
@@ -2,7 +2,6 @@ package com.gp.arqueras_javafx;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,9 +9,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import org.apache.log4j.BasicConfigurator;
 
 import java.io.File;
 
