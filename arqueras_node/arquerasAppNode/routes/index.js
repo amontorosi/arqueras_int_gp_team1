00e3dda920d10a3ac49b2fcea8c318a2d49bdd7a
Manuel Valdepeñas <manuelvdlt99@gmail.com>
2024-06-10 12:49:04 +0200

    Basic web design done

--- arqueras_node/arquerasAppNode/routes/index.js
+++ arqueras_node/arquerasAppNode/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var {Control} = require("../Control");
+
+const controlador = new Control();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  controlador.testDBQuery((results) => {
+    /*if (err) {
+      // Maneja el error
+      console.error('Error al realizar la consulta:', err);
+      return;
+    }*/
+    // Haz algo con los resultados si es necesario
+    //console.log('Resultado de la consulta:', results);
+    res.render("index", {
+      //datos: results
+      datos: results
+    });
+  });
+  //res.render('index', { title: 'Prueba' });
+  
 });
 
 module.exports = router;
