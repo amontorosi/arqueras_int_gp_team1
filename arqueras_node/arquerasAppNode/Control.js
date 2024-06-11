b8a18d27d1c166ddb3e46aacafc779520ffec226
Manuel Valdepeñas <manuelvdlt99@gmail.com>
2024-06-11 12:41:46 +0200

    Added table formatting functions + empty info row if DB has no data

--- arqueras_node/arquerasAppNode/Control.js
+++ arqueras_node/arquerasAppNode/Control.js
@@ -10,15 +10,10 @@ class Control {
     scoreQuery(callback) {
         var query1 = `select * from scores order by score desc`
         this.basedatos.consulta(query1, (resultado) => {
-            if (resultado.length == 0) {
-                console.log("No hay datos");
-                callback('0')
-            } else {
-                //var datos=JSON.stringify(resultado)
-                console.log('Consulta realizada!');
-                console.log(resultado);
-                callback(resultado);
-            }
+            //var datos=JSON.stringify(resultado)
+            console.log('Consulta realizada!');
+            console.log(resultado);
+            callback(resultado);
         });
     }
 }
---
+++ arqueras_node/arquerasAppNode/public/js/scripts.js
@@ -0,0 +1,25 @@
+function formatDate(d) {
+    var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+    return d.getDate() + " de " + meses[d.getMonth() - 1] + " de " + d.getFullYear() + " (" + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + ")";
+}
+
+function formatDiff(d) {
+    var diffSpanish;
+    switch (d) {
+        case "easy":
+            diffSpanish = "Fácil";
+            break;
+        case "normal":
+            diffSpanish = "Normal";
+            break;
+        case "hard":
+            diffSpanish = "Difícil";
+            break;
+    }
+    return diffSpanish;
+}
+
+module.exports = {
+    formatDate: formatDate,
+    formatDiff: formatDiff
+}
